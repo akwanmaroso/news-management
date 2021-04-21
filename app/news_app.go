f6efe65a1394672d73fdb1f5bb45efefe6824d59
akwanmaroso <akwanmaroso@gmail.com>
2021-04-21 17:49:16 +0800

    feat: app layer

---
+++ app/news_app.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"github.com/akwanmaroso/news/domain/entity"
+	"github.com/akwanmaroso/news/domain/repository"
+)
+
+type newsApps struct {
+	newsApp repository.NewsRepository
+}
+
+type NewsAppInterface interface {
+	SaveNews(*entity.News) (*entity.News, map[string]string)
+	GetAllNews() ([]*entity.News, error)
+	GetNews(uint64) (*entity.News, error)
+	UpdateNews(*entity.News) ([]*entity.News, map[string]string)
+	DeleteNews(uint64) error
+}
+
+var _ NewsAppInterface = &newsApps{}
+
+func (n *newsApps) SaveNews(news *entity.News) (*entity.News, map[string]string) {
+	return n.newsApp.SaveNews(news)
+}
+
+func (n *newsApps) GetAllNews() ([]*entity.News, error) {
+	return n.newsApp.GetAllNews()
+}
+
+func (n *newsApps) GetNews(newsId uint64) (*entity.News, error) {
+	return n.newsApp.GetNews(newsId)
+}
+
+func (n *newsApps) UpdateNews(news *entity.News) ([]*entity.News, map[string]string) {
+	return n.newsApp.UpdateNews(news)
+}
+
+func (n *newsApps) DeleteNews(newsId uint64) error {
+	return n.newsApp.DeleteNews(newsId)
+}
---
+++ app/tag_app.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"github.com/akwanmaroso/news/domain/entity"
+	"github.com/akwanmaroso/news/domain/repository"
+)
+
+type tagApps struct {
+	tagApp repository.TagRepository
+}
+
+type TagAppInterface interface {
+	SaveTag(*entity.Tag) (*entity.Tag, map[string]string)
+	GetTag(uint64) (*entity.Tag, error)
+	GetAllTag() ([]*entity.Tag, error)
+	UpdateTag(*entity.Tag) (*entity.Tag, map[string]string)
+	DeleteTag(uint64) error
+}
+
+var _ TagAppInterface = &tagApps{}
+
+func (t *tagApps) SaveTag(tag *entity.Tag) (*entity.Tag, map[string]string) {
+	return t.tagApp.SaveTag(tag)
+}
+
+func (t *tagApps) GetTag(tagId uint64) (*entity.Tag, error) {
+	return t.tagApp.GetTag(tagId)
+}
+
+func (t *tagApps) GetAllTag() ([]*entity.Tag, error) {
+	return t.tagApp.GetAllTag()
+}
+
+func (t *tagApps) UpdateTag(tag *entity.Tag) (*entity.Tag, map[string]string) {
+	return t.tagApp.UpdateTag(tag)
+}
+
+func (t *tagApps) DeleteTag(tagId uint64) error {
+	return t.tagApp.DeleteTag(tagId)
+}
