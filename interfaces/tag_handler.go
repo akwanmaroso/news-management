86cca20506add019185ae98b7e0c98eb1399af46
akwanmaroso <akwanmaroso@gmail.com>
2021-04-22 18:02:00 +0800

    feat: add Tag api handler

---
+++ interfaces/tag_handler.go
@@ -0,0 +1,127 @@
+package interfaces
+
+import (
+	"fmt"
+	"github.com/akwanmaroso/news/app"
+	"github.com/akwanmaroso/news/domain/entity"
+	. "github.com/akwanmaroso/news/infrastructure/responses"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type Tag struct {
+	tagApp  app.TagAppInterface
+	newsApp app.NewsAppInterface
+}
+
+func NewTag(tagApp app.TagAppInterface, newsApp app.NewsAppInterface) *Tag {
+	return &Tag{tagApp: tagApp, newsApp: newsApp}
+}
+
+func (t *Tag) SaveTag(c *gin.Context) {
+	var tag entity.Tag
+	err := c.BindJSON(&tag)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, ErrorResponse(http.StatusUnprocessableEntity, "JSON not valid"))
+		return
+	}
+
+	emptyTag := entity.Tag{}
+	emptyTag.Name = tag.Name
+	validateTag := emptyTag.Validate()
+	if len(validateTag) > 0 {
+		c.JSON(http.StatusBadRequest, ErrorResponse(http.StatusBadRequest, validateTag))
+		return
+	}
+
+	newTag := entity.Tag{}
+	newTag.Name = tag.Name
+	savedTag, saveErr := t.tagApp.SaveTag(&newTag)
+	if saveErr != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse(http.StatusInternalServerError, saveErr))
+		return
+	}
+
+	c.JSON(http.StatusOK, SuccessResponse(http.StatusOK, savedTag))
+}
+
+func (t *Tag) GetAllTag(c *gin.Context) {
+	tags, err := t.tagApp.GetAllTag()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse(http.StatusInternalServerError, err))
+		return
+	}
+
+	c.JSON(http.StatusOK, SuccessResponse(http.StatusOK, tags))
+}
+
+func (t *Tag) GetTag(c *gin.Context) {
+	tagId, err := strconv.ParseUint(c.Param("tag_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse(http.StatusBadRequest, err.Error()))
+		return
+	}
+	tag, err := t.tagApp.GetTag(tagId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse(http.StatusInternalServerError, err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, SuccessResponse(http.StatusOK, tag))
+}
+
+func (t *Tag) UpdateTag(c *gin.Context) {
+	tagId, err := strconv.ParseUint(c.Param("news_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse(http.StatusBadRequest, err.Error()))
+		return
+	}
+
+	var tag entity.Tag
+	err = c.BindJSON(&tag)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, ErrorResponse(http.StatusUnprocessableEntity, "JSON not valid"))
+		return
+	}
+
+	emptyTag := entity.Tag{}
+	emptyTag.Name = tag.Name
+	validateTag := emptyTag.Validate()
+	if len(validateTag) > 0 {
+		c.JSON(http.StatusBadRequest, ErrorResponse(http.StatusBadRequest, validateTag))
+		return
+	}
+
+	tagToUpdate, err := t.tagApp.GetTag(tagId)
+	if err != nil {
+		c.JSON(http.StatusNotFound, ErrorResponse(http.StatusNotFound, fmt.Sprintf("tag with id: %d is not exists", tagId)))
+		return
+	}
+
+	tagToUpdate.Name = tag.Name
+	updatedTag, updateError := t.tagApp.UpdateTag(&tag)
+	if updateError != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse(http.StatusInternalServerError, updateError))
+		return
+	}
+
+	c.JSON(http.StatusOK, SuccessResponse(http.StatusOK, updatedTag))
+}
+
+func (t *Tag) DeleteTag(c *gin.Context) {
+	tagId, err := strconv.ParseUint(c.Param("tag_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse(http.StatusBadRequest, err.Error()))
+		return
+	}
+
+	err = t.tagApp.DeleteTag(tagId)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse(http.StatusInternalServerError, err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, SuccessResponse(http.StatusOK, fmt.Sprintf("News With Tag: %d is succesfully deleted", tagId)))
+}
