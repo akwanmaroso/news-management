e1c55c1d6eb14c42027aa07da41f8cff144afed1
akwanmaroso <akwanmaroso@gmail.com>
2021-04-23 01:42:19 +0800

    ref(tag): add some validation

--- domain/entity/tag.go
+++ domain/entity/tag.go
@@ -1,9 +1,27 @@
 package entity
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"html"
+	"strings"
+)
 
 type Tag struct {
 	gorm.Model
-	NewsID uint64 `json:"news_id"`
-	Name   string `json:"name"`
+	Name string  `gorm:"not null;unique" json:"name"`
+	News []*News `gorm:"many2many:news_tags" json:"news"`
+}
+
+func (tag *Tag) BeforeSave() {
+	tag.Name = html.EscapeString(strings.TrimSpace(tag.Name))
+}
+
+func (tag *Tag) Validate() map[string]string {
+	var errorMessage = make(map[string]string)
+
+	if tag.Name == "" || tag.Name == "null" {
+		errorMessage["name_required"] = "Name of tag can't be null"
+	}
+
+	return errorMessage
 }
