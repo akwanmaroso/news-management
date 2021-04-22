d0da3e2d3abad6a24f93cbc8c891803965c6070a
akwanmaroso <akwanmaroso@gmail.com>
2021-04-22 18:05:27 +0800

    feat: add new logic

--- app/news_app.go
+++ app/news_app.go
@@ -13,7 +13,9 @@ type NewsAppInterface interface {
 	SaveNews(*entity.News) (*entity.News, map[string]string)
 	GetAllNews() ([]*entity.News, error)
 	GetNews(uint64) (*entity.News, error)
-	UpdateNews(*entity.News) ([]*entity.News, map[string]string)
+	GetByTopic(string) ([]*entity.News, error)
+	GetByStatus(string) ([]*entity.News, error)
+	UpdateNews(*entity.News) (*entity.News, map[string]string)
 	DeleteNews(uint64) error
 }
 
@@ -31,7 +33,15 @@ func (n *newsApps) GetNews(newsId uint64) (*entity.News, error) {
 	return n.newsApp.GetNews(newsId)
 }
 
-func (n *newsApps) UpdateNews(news *entity.News) ([]*entity.News, map[string]string) {
+func (n *newsApps) GetByTopic(topic string) ([]*entity.News, error) {
+	return n.newsApp.GetByTopic(topic)
+}
+
+func (n *newsApps) GetByStatus(status string) ([]*entity.News, error) {
+	return n.newsApp.GetByStatus(status)
+}
+
+func (n *newsApps) UpdateNews(news *entity.News) (*entity.News, map[string]string) {
 	return n.newsApp.UpdateNews(news)
 }
 
