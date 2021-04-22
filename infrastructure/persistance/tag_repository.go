80bd72362e787e47303144a18262bb35f432c8f7
akwanmaroso <akwanmaroso@gmail.com>
2021-04-22 22:49:10 +0800

    feat: add tag logic

---
+++ infrastructure/persistance/tag_repository.go
@@ -0,0 +1,98 @@
+package persistance
+
+import (
+	"errors"
+	"github.com/akwanmaroso/news/domain/entity"
+	"github.com/akwanmaroso/news/domain/repository"
+	"github.com/jinzhu/gorm"
+	"strings"
+)
+
+type TagRepo struct {
+	db *gorm.DB
+}
+
+func NewTagRepository(db *gorm.DB) *TagRepo {
+	return &TagRepo{db}
+}
+
+var _ repository.TagRepository = &TagRepo{}
+
+func (t *TagRepo) SaveTag(tag *entity.Tag) (*entity.Tag, map[string]string) {
+	dbErr := map[string]string{}
+	err := t.db.Debug().Create(&tag).Error
+	if err != nil {
+		if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
+			dbErr["unique_name"] = "tag already taken"
+		}
+		dbErr["db_error"] = "database error"
+		return nil, dbErr
+	}
+	return tag, nil
+}
+
+func (t *TagRepo) GetTag(tagId uint64) (*entity.Tag, error) {
+	var tag entity.Tag
+	err := t.db.Debug().Where("id = ?", tagId).Take(&tag).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, errors.New("record tag not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &tag, nil
+}
+
+func (t *TagRepo) GetAllTag() ([]*entity.Tag, error) {
+	var tag []*entity.Tag
+	err := t.db.Debug().Limit(100).Preload("News").Order("created_at desc").Find(&tag).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, errors.New("record tag not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return tag, nil
+}
+
+func (t *TagRepo) UpdateTag(tag *entity.Tag) (*entity.Tag, map[string]string) {
+	dbErr := map[string]string{}
+	err := t.db.Debug().Save(&tag).Error
+	if err != nil {
+		if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
+			dbErr["unique_name"] = "topic already taken"
+			return nil, dbErr
+		}
+		dbErr["db_error"] = "database error"
+		return nil, dbErr
+	}
+	return tag, nil
+}
+
+func (t *TagRepo) DeleteTag(tagId uint64) error {
+	var tag entity.Tag
+
+	err := t.db.Debug().Where("id = ?", tagId).Delete(&tag).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return errors.New("record news not found")
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TagRepo) FindTagByName(name string) (*entity.Tag, error) {
+	var tag entity.Tag
+
+	err := t.db.Debug().Where("name = ?", name).Take(&tag).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, errors.New("record news not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &tag, nil
+}
